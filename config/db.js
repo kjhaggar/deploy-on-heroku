7ae56819dbe374f25efda03c0873cfb2981f6ee9
kjhaggar <kjhaggar@bestpeers.com>
2019-08-05 18:04:28 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/deploySample', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/deploySample', { useNewUrlParser: true });
 
-// mongoose.connection.on('connected', function (err) {
-//   console.log("Connected to DB Successfully");
-// });
+mongoose.connection.on('connected', function (err) {
+  console.log("Connected to DB Successfully");
+});
 
-// // Error handler
-// mongoose.connection.on('error', function (err) {
-//   console.log(err);
-// });
+// Error handler
+mongoose.connection.on('error', function (err) {
+  console.log(err);
+});
